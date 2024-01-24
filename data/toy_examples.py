fixed the no gradient error

data/toy_examples.py
@@ -8,15 +8,15 @@ def create_geodesic_pairs_circle(N,time_steps, noise = 0):
     space = Hypersphere(dim = 1)
     t = torch.linspace(0,1,time_steps)
     start_points = space.random_point(n_samples=N)
-    omega = torch.rand((N,1))*(2*np.pi/50)
+    omega = torch.rand((N,1))*(np.pi/5) 
     start_tangent_vecs = torch.stack([-1*start_points[:,1] , start_points[:,0]], axis = -1)*omega
     geodesic = space.metric.geodesic(initial_point=start_points, initial_tangent_vec = start_tangent_vecs)
     geodesic_trajectories = torch.unsqueeze(geodesic(t),0) if N == 1 else geodesic(t)
     geodesic_trajectories += torch.randn(*geodesic_trajectories.shape) * noise
 
-    val_size = int(N/2)
+    val_size = 2*N
     val_start_points = space.random_point(n_samples=val_size)
-    val_omega = torch.rand((val_size,1))*(2*np.pi/50)
+    val_omega = torch.rand((val_size,1))*(np.pi/5) 
     val_start_tangent_vecs = torch.stack([-1*val_start_points[:,1] , val_start_points[:,0]], axis = -1)*val_omega
     val_geodesic = space.metric.geodesic(initial_point=val_start_points, initial_tangent_vec = val_start_tangent_vecs)
     val_geodesic_trajectories = torch.unsqueeze(val_geodesic(t),0) if N == 1 else val_geodesic(t)
@@ -24,4 +24,23 @@ def create_geodesic_pairs_circle(N,time_steps, noise = 0):
     return geodesic_trajectories, start_points, start_tangent_vecs, val_geodesic_trajectories, val_start_points, val_start_tangent_vecs
 
 
+def create_geodesic_pairs_circle_hemisphere(N, time_steps, noise = 0):
+    torch.set_default_dtype(torch.float32)
+    space = Hypersphere(dim = 1)
+    t = torch.linspace(0,1,time_steps)
+    theta = torch.rand((N,1)) * np.pi/4 ### sample angle in right quadrant 
+    omega = torch.rand((N,1))*(np.pi/5)  ## sample 
+    start_points = space.angle_to_extrinsic(theta)
+    start_tangent_vecs = torch.stack([-1*start_points[:,1] , start_points[:,0]], axis = -1)*omega
+    geodesic = space.metric.geodesic(initial_point=start_points, initial_tangent_vec = start_tangent_vecs)
+    geodesic_trajectories = torch.unsqueeze(geodesic(t),0) if N == 1 else geodesic(t)
+    geodesic_trajectories += torch.randn(*geodesic_trajectories.shape) * noise
+
+    val_start_points = space.angle_to_extrinsic(theta + np.pi)
+    val_omega = omega
+    val_start_tangent_vecs = torch.stack([-1*val_start_points[:,1] , val_start_points[:,0]], axis = -1)*val_omega
+    val_geodesic = space.metric.geodesic(initial_point=val_start_points, initial_tangent_vec = val_start_tangent_vecs)
+    val_geodesic_trajectories = torch.unsqueeze(val_geodesic(t),0) if N == 1 else val_geodesic(t)
+    val_geodesic_trajectories += torch.randn(*val_geodesic_trajectories.shape) * noise
+    return geodesic_trajectories, start_points, start_tangent_vecs, val_geodesic_trajectories, val_start_points, val_start_tangent_vecs
     

main.py
@@ -4,7 +4,7 @@ import torch
 from typing import List
 from models.train import train
 from geomstats.geometry.hypersphere import Hypersphere
-from data.toy_examples import create_geodesic_pairs_circle
+from data.toy_examples import create_geodesic_pairs_circle, create_geodesic_pairs_circle_hemisphere
 from visualization.visualize import visualize_circle_metric, visualize_training_data, visualize_loss, visualize_convergence
 
 
@@ -16,6 +16,49 @@ def plot_convergence(preds: List[np.ndarray], actual: np.ndarray, skip_every: in
     for index in indices:
         visualize_convergence(torch.reshape(preds[index],(-1, 2)), actual,epoch_num=index,n=n, penalty=penalty, val = val)
     
+
+def circle_metric_hemisphere_with_n(sample_sizes: List[int], noise: float, penalty: float, timesteps:int, keep_preds:bool = False, val:bool = True):
+    theta = np.linspace(0, np.pi * 2, 1000)
+    basis_on_manifold = torch.from_numpy(np.vstack([np.cos(theta), np.sin(theta)]).T).to(torch.float32)
+    xi, yi = torch.meshgrid(torch.linspace(-1.5,1.5,50), torch.linspace(-1.5,1.5,50))
+    manifold_basis = torch.stack([xi.flatten(), yi.flatten()], axis = -1)
+    m = [5,5]
+    c = 4.0
+    active_dims = [0,1]
+    n_dims = len(active_dims)
+
+    model_loss = []
+    frechet_loss = []
+    latent_space = Hypersphere(dim = 1, equip=True)
+    for num_samps in sample_sizes:
+        torch.set_default_dtype(torch.float32)
+        trajectories, start_points, start_velo, val_trajectories, val_start_points, val_start_velo = create_geodesic_pairs_circle_hemisphere(num_samps, timesteps, noise = noise)
+        sample_basis = torch.reshape(trajectories,(-1, n_dims)) ### only construct basis from whatever points we have 
+        val_sample_basis = torch.reshape(val_trajectories,(-1, n_dims))
+        initial_conditions = (start_points, start_velo)
+        val_initial_conditions = (val_start_points, val_start_velo)
+        visualize_training_data(sample_basis, num_samps, start_velo)
+        model, preds = train(trajectories, initial_conditions, epochs = 300, regularizer=penalty, n = n_dims,
+                       t = timesteps, m = m, c = c, val_initial_conditions=val_initial_conditions, val_input_trajectories=val_trajectories,
+                  basis = manifold_basis.to(torch.float32), active_dims = active_dims, return_preds=keep_preds, val=val)
+        
+        with torch.no_grad():
+            generated_trajectories, _ = model.forward(initial_conditions)
+            predicted_trajectories = torch.permute(generated_trajectories, (1,0,2))
+            visualize_circle_metric(model, basis_on_manifold, num_samps, penalty)
+            visualize_training_data(torch.reshape(predicted_trajectories, (-1, n_dims)), num_samps, penalty = penalty, train = False)
+            
+
+            val_generated_trajectories, _ = model.forward(val_initial_conditions)
+            val_predicted_trajectories = torch.permute(val_generated_trajectories, (1,0,2))
+            plot_convergence([torch.reshape(val_predicted_trajectories, (-1, n_dims))], val_sample_basis,n = num_samps, penalty = penalty, skip_every = 10, val = val)
+
+            geodesic_distance = latent_space.metric.dist(latent_space.projection(torch.reshape(predicted_trajectories, (-1, n_dims))), sample_basis).sum()/num_samps
+            frechet_loss.append(geodesic_distance)
+            model_loss.append(torch.square(predicted_trajectories - trajectories).sum()/num_samps)
+        if keep_preds:
+            plot_convergence(preds, sample_basis, skip_every=30, n = num_samps, penalty = penalty )
+    # visualize_loss(model_loss, frechet_loss, sample_sizes)
 
 
 def circle_metric_with_n(sample_sizes: List[int], noise: float, penalty: float, timesteps:int, keep_preds:bool = False, val:bool = True):
@@ -59,7 +102,9 @@ def circle_metric_with_n(sample_sizes: List[int], noise: float, penalty: float, 
             model_loss.append(torch.square(predicted_trajectories - trajectories).sum()/num_samps)
         if keep_preds:
             plot_convergence(preds, sample_basis, skip_every=30, n = num_samps, penalty = penalty )
-    visualize_loss(model_loss, frechet_loss, sample_sizes)
+    # visualize_loss(model_loss, frechet_loss, sample_sizes)
+
+
     
 
 
@@ -79,10 +124,15 @@ if __name__ == "__main__":
     parser.add_argument('--val', action="store_true", help='whether or not to compute validation loss', default=True)
     parser.add_argument('--timesteps', type=int,  help='length of trajectories')
     parser.add_argument('--sample_sizes',type=lambda x: [int(item) for item in x.split(",")], help='comma separated list of ints')
+    parser.add_argument('--hemisphere', action="store_true", help="fit one part of the manifold", default = False)
     args = parser.parse_args()
 
     torch.manual_seed(12)
-    circle_metric_with_n(sample_sizes = args.sample_sizes, noise = args.noise, penalty = args.penalty, keep_preds=args.keep_preds, timesteps=args.timesteps)
+    if args.hemisphere:
+        circle_metric_hemisphere_with_n(sample_sizes = args.sample_sizes, noise = args.noise, penalty = args.penalty, keep_preds=args.keep_preds, timesteps=args.timesteps)
+
+    else:
+        circle_metric_with_n(sample_sizes = args.sample_sizes, noise = args.noise, penalty = args.penalty, keep_preds=args.keep_preds, timesteps=args.timesteps)
 
 
 

models/model.py
@@ -1,5 +1,5 @@
 import torch
-from torchdiffeq import odeint_adjoint as odeint
+from torchdiffeq import odeint as odeint
 from typing import List, Tuple
 import torch.nn as nn
 from .hsgp import HSGPExpQuadWithDerivative
@@ -11,9 +11,10 @@ class _ODEFunc(nn.Module):
     def __init__(self, module: GPRiemannianEuclidean):
         super(_ODEFunc, self).__init__()
         self.module = module
+    @torch.enable_grad()
     def forward(self, t, x):
-
-        return self.module.metric.geodesic_equation(x, t)
+        ode_eq = self.module.metric.geodesic_equation(x, t)
+        return ode_eq
 
 
 class ODEBlock(nn.Module):
@@ -26,9 +27,9 @@ class ODEBlock(nn.Module):
         self.solver = solver
 
  
-
+    
     def forward(self, X: Tuple[torch.Tensor, torch.Tensor], integration_time):
-     
+
         out = odeint(
             self.odefunc, X, integration_time, rtol=self.rtol,
             atol=self.atol, method=self.solver)
@@ -39,7 +40,7 @@ class RiemannianAutoencoder(nn.Module):
     def __init__(self, n: int,t: int,  m: List[int], c: float, regularization: float, basis, active_dims: List):
         super(RiemannianAutoencoder, self).__init__()
         d = int(n*(n+1)/2)
-        self.gp_components = [HSGPExpQuadWithDerivative(m, c, active_dims) for _ in range(d)]
+        self.gp_components = nn.ModuleList([HSGPExpQuadWithDerivative(m, c, active_dims) for _ in range(d)])
         for gp_component in self.gp_components:
             gp_component.prior_linearized(basis) ### initialize basis
         self.metric_space = GPRiemannianEuclidean(n, self.gp_components)
@@ -50,6 +51,7 @@ class RiemannianAutoencoder(nn.Module):
         self.regularization = regularization
         self.basis = basis
 
+    @torch.enable_grad()
     def forward(self,initial_conditions):
         time_steps = torch.linspace(0.0,1.0,self.t)
         predicted_vals = self.ode_layer.forward(initial_conditions, time_steps)
@@ -57,8 +59,7 @@ class RiemannianAutoencoder(nn.Module):
 
     
     def loss(self, predicted_vals, actual_vals, val = False):
-
-        reconstruction_loss =  torch.sum(torch.square(predicted_vals - actual_vals), (1,2)).mean()
+        reconstruction_loss = nn.MSELoss()(predicted_vals,actual_vals)
         if not val:
             reconstruction_loss = reconstruction_loss + self.parameter_loss()
         if self.regularization > 0:
@@ -69,8 +70,8 @@ class RiemannianAutoencoder(nn.Module):
     def parameter_loss(self):
         parameter_loss = torch.FloatTensor([0.0])
         for gp in self.gp_components:
-            parameter_loss += torch.square(gp._beta).sum()
-            parameter_loss += torch.sum(gp.ls)
+            parameter_loss += torch.square(gp._beta).sum()/len(self.gp_components)
+            parameter_loss += torch.sum(gp.ls)/len(self.gp_components)
         return parameter_loss
 
     

models/train.py
@@ -50,28 +50,29 @@ def train(input_trajectories, initial_conditions: Tuple[torch.Tensor, torch.Tens
           val_initial_conditions:Tuple[torch.Tensor, torch.Tensor],  epochs, 
           regularizer:float, n, t, m:List[int], c:float, basis, active_dims: List, return_preds:bool = False, val: bool = False):
     
-    early_stopping = EarlyStopping(patience=3, verbose=True)
+    # early_stopping = EarlyStopping(patience=3, verbose=True)
     model = RiemannianAutoencoder(n,t,m,c,regularizer, basis, active_dims)
-    param_list = []
-    for gp in model.gp_components:
-        param_list += list(gp.parameters())
-    optimizer = optim.Adam(param_list, lr=0.01)
-    
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+
     preds = []
+    with torch.no_grad():
+        predicted_trajectories, _ = model.forward(initial_conditions)
+        preds.append(torch.permute(predicted_trajectories.detach(), (1,0,2)))
     for epoch in range(epochs):
         optimizer.zero_grad()
         # Forward pass
         predicted_trajectories, _ = model.forward(initial_conditions)
         loss = model.loss(torch.permute(predicted_trajectories.float(), (1,0,2)), input_trajectories.float())
         # Backward pass and optimization
-        
         loss.backward(retain_graph=True)
+        
         optimizer.step()
 
         if val:
+            model.eval()
             with torch.no_grad():
                 predicted_val_trajectories, _ = model.forward(val_initial_conditions)
-                val_loss = model.loss(torch.permute(predicted_val_trajectories.float(), (1,0,2)), val_input_trajectories.float(), val = True)
+                val_loss = model.loss(torch.permute(predicted_val_trajectories.float().detach(), (1,0,2)), val_input_trajectories.float(), val = True)
         else:
             val_loss = "None"
         if return_preds: